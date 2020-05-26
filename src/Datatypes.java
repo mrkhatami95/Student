8feb48741ae36ee0344bae1e9f6e5eeb25a72bde
mrkhatami <mr.khatami95@gmail.com>
2020-05-26 12:43:47 +0430

    first commit

---
+++ src/Datatypes.java
@@ -0,0 +1,46 @@
+/**
+ * Created by MRK on 5/22/2020.
+ */
+public class Datatypes {
+
+    public static void main(String[] args) {
+
+        /*
+            ****
+            ***
+            **
+            *
+        */
+
+        for(int i = 0; i < 4 ; i++){
+
+            for(int j = 4 ; j > i ; j--){
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+        }
+
+        /*Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+
+        int x = 0;
+
+        Integer x2 = 0;
+*/
+
+
+        /*System.out.println("byte min value is : " + Byte.MIN_VALUE + "byte max value is : " + Byte.MAX_VALUE);
+        System.out.println("short min value is : " + Short.MIN_VALUE + "short max value is : " + Short.MAX_VALUE);
+        System.out.println("int min value is : " + Integer.MIN_VALUE + "int max value is : " + Integer.MAX_VALUE);
+        System.out.println("long min value is : " + Long.MIN_VALUE + "long max value is : " + Long.MAX_VALUE);
+        System.out.println("float min value is : " + Float.MIN_VALUE + "float max value is : " + Float.MAX_VALUE);
+        System.out.println("double min value is : " + Double.MIN_VALUE + "double max value is : " + Double.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE + Character.MAX_VALUE); -- description!  */
+
+
+    }
+
+}
